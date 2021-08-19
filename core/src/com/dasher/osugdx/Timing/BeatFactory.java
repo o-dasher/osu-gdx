67ddf6b0b4db5093ebcf5b06b8ba5f1dfd4bda09
o-dasher <88356162+o-dasher@users.noreply.github.com>
2021-08-19 10:56:40 -0400

    b

--- core/src/com/dasher/osugdx/Timing/BeatFactory.java
+++ core/src/com/dasher/osugdx/Timing/BeatFactory.java
@@ -6,6 +6,8 @@ import com.dasher.osugdx.Framework.Interfaces.UpdateAble;
 import com.dasher.osugdx.IO.Beatmaps.BeatmapManager;
 import com.dasher.osugdx.IO.Beatmaps.BeatmapManagerListener;
 
+import org.jetbrains.annotations.NotNull;
+
 import lt.ekgame.beatmap_analyzer.beatmap.Beatmap;
 import lt.ekgame.beatmap_analyzer.beatmap.TimingPoint;
 
@@ -28,14 +30,19 @@ public class BeatFactory implements Listenable<BeatListener>, UpdateAble, BeatLi
 
     @Override
     public void update(float delta) {
-        if (beatmapManager.getCurrentMap() == null) {
+        Beatmap beatmap = beatmapManager.getCurrentMap();
+
+        if (beatmap == null) {
             return;
         }
+
         if (timeSinceLastBeat == 0) {
             timeSinceLastBeat = beatmapManager.getTimeLastMap();
         }
+
         Array<TimingPoint> timingPoints = beatmapManager.getCurrentMap().getTimingPoints();
         double deltaSinceLastMap = (System.nanoTime() - beatmapManager.getTimeLastMap()) / 1e6;
+
         if (currentTimingPoint == null) {
             for (TimingPoint timingPoint: timingPoints) {
                 if (!timingPoint.isInherited()) {
@@ -43,29 +50,48 @@ public class BeatFactory implements Listenable<BeatListener>, UpdateAble, BeatLi
                 }
             }
         }
+
         while (currentTimingPointIndex < timingPoints.size - 1 && timingPoints.get(currentTimingPointIndex + 1).getTimestamp() <= deltaSinceLastMap) {
             currentTimingPointIndex++;
         }
+
         TimingPoint newTimingPoint = timingPoints.get(currentTimingPointIndex);
         if (!newTimingPoint.isInherited()) {
             currentTimingPoint = newTimingPoint;
         }
-        if ((System.nanoTime() - timeSinceLastBeat) / 1e6 > currentTimingPoint.getBeatLength()) {
+
+        double beatLength = currentTimingPoint.getBeatLength();
+        double timePassedSinceLastBeat = (System.nanoTime() - timeSinceLastBeat) / 1e6;
+
+        if (timePassedSinceLastBeat > beatLength) {
             timeSinceLastBeat = System.nanoTime();
             onNewBeat(currentTimingPoint);
         }
     }
 
+    public TimingPoint getBeatFor(@NotNull BeatListener beatListener, @NotNull TimingPoint timingPoint) {
+        TimingPoint clone = timingPoint.clone();;
+        clone.setBeatLength(timingPoint.getBeatLength() / beatListener.getBeatDivisor());
+        return clone;
+    }
+
     @Override
     public void onNewBeat(TimingPoint timingPoint) {
         for (BeatListener listener: listeners) {
-            listener.onNewBeat(timingPoint);
+            listener.onNewBeat(getBeatFor(listener, timingPoint));
         }
     }
 
     @Override
     public void onFourthBeat(TimingPoint timingPoint) {
+        for (BeatListener listener: listeners) {
+            listener.onNewBeat(getBeatFor(listener, timingPoint));
+        }
+    }
 
+    @Override
+    public double getBeatDivisor() {
+        return 1;
     }
 
     @Override
--- core/src/com/dasher/osugdx/Timing/BeatListener.java
+++ core/src/com/dasher/osugdx/Timing/BeatListener.java
@@ -3,6 +3,7 @@ package com.dasher.osugdx.Timing;
 import lt.ekgame.beatmap_analyzer.beatmap.TimingPoint;
 
 public interface BeatListener {
+    default double getBeatDivisor() { return 1; }
     void onFourthBeat(TimingPoint timingPoint);
     void onNewBeat(TimingPoint timingPoint);
 }
